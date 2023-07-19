a4fa284911795ba0a8b4d6dac2fba96dd1df84ed
KeniLazarus <milojkovic.stanko@gmail.com>
2023-07-19 03:13:15 +0200

    deleting unecessary files

--- fakerData.js
+++
@@ -1,8 +0,0 @@
-import { faker } from 'faker-js/faker'
-
-const randomFirstName = faker.person.Name();
-const randomLastName = faker.person.LastName();
-const randomEmail = faker.internet.email();
-const randomZipCode = faker.internet.ZipCode();
-
-const user = createRandomUser();
